075e2e8bbabc380c8112ffe058727a1f6d60b0b2
NNASTYA2007 <advokat1707@gmail.com>
2022-01-20 22:00:26 +0200

    renderDone

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,42 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const wraper = document.querySelector('.gallery');
+// console.log(wraper);
+
+
+
+let itemOfgallery = galleryItems.map((image) => {
+    console.log(image);
+   
+    return `<div class="gallery__item">
+<a class="gallery__link" href="${image.original}">
+<img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+</a>
+
+</div>`
+    
+
+}).join('');
+
+wraper.insertAdjacentHTML("afterbegin", itemOfgallery); 
+// console.log(...itemOfgallrry);
+/**
+const getImages = document.querySelector('.gallery');
+getImages.style.display = 'flex';
+getImages.style.justifyContent = 'space-between';
+getImages.style.listStyle = 'none';
+
+let itemOfImage = images.map((image) => {
+  return `<li><img src="${image.url}" alt="${image.alt}" width='400px' height='260px'/></li>`;
+  
+}).join('');
+// console.log(itemOfImage);
+getImages.insertAdjacentHTML("afterbegin", itemOfImage); 
+*/ 
